f8b02822f63e57c67690c0a5dbcafbeef8abb22d
richod1 <frimpsup@gmail.com>
2023-10-27 06:41:58 -0400

    doubly-linked added

--- linkedList.js
+++ linkedList.js
@@ -1,3 +1,6 @@
+
+// singly linked list
+
 class Node{
     constructor(data){
         this.data=data;
@@ -27,4 +30,64 @@ let current=head
 while(current !== null){
     console.log(current.data)
     current=current.next;
+}
+
+
+console.log("-----------")
+
+// doubly linked list
+class DoublyNode{
+    constructor(data){
+        this.data=data;
+        this.next=null;
+        this.prev=null;
+    }
+}
+
+let nodes1=new DoublyNode(10)
+let nodes2=new DoublyNode(20)
+let nodes3=new DoublyNode(30)
+
+
+// linking nodes
+nodes1.next=nodes2;
+nodes2.next=nodes3;
+
+// linking previous nodes
+nodes2.prev=nodes1;
+nodes3.prev=nodes2;
+
+
+// insert new node to linkedlist
+const newNodes=new Node(15)
+
+newNodes.next=nodes2;
+newNodes.prev=nodes1;
+
+nodes1.next=newNodes;
+nodes2.prev=newNodes;
+
+// delete node2
+nodes1.next=nodes3;
+nodes3.prev=nodes1;
+nodes2=null;
+
+// assume head is node1 
+const heads=nodes1;
+
+// traversing doubly linked
+
+
+let currents=heads
+while(currents !== null){
+    console.log(currents.data)
+    currents=currents.next;
+}
+
+
+console.log("---prev----")
+currents=nodes3
+while(currents !== null){
+    console.log(currents.data)
+    currents=currents.prev
 }
